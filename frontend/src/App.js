a8dac929b681e22b202bbedc3ba5df64c3b89003
Dare Ionut Daniel <dareionutdaniel@yahoo.com>
2024-02-08 20:03:48 +0200

    Refactorin frontend folder structure. Preparing the frontend for Kanban Project.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Container from "@mui/material/Container";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
-import Home from "./screens/Home";
-import { LeftMenu } from "./components/LeftMenu";
-import { Grid } from "@mui/material";
-import { NavBar } from "./components/NavBar";
-
-const darkTheme = createTheme({
-  palette: {
-    mode: "dark",
-  },
-});
+import { Kanban } from "./Kanban";
 
 const App = () => {
   return (
-    <div id="main-containter">
-      <BrowserRouter>
-        <ThemeProvider theme={darkTheme}>
-          <CssBaseline />
-          <NavBar />
-          <div id="application-container">
-            <Grid container spacing={2}>
-              <Grid item xs={6} md={2}>
-                <LeftMenu />
-              </Grid>
-              <Grid item xs={6} md={10}>
-                <Container fixed>
-                  <Routes>
-                    <Route path="/" element={<Home />} />
-                  </Routes>
-                </Container>
-              </Grid>
-            </Grid>
-          </div>
-        </ThemeProvider>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Kanban />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
